12d37f72974145c9f6419d8af1041c3137628dc3
andy <andrea.tol.flores@gmail.com>
2023-04-26 18:56:16 -0600

    Se agregó el Trapecio

--- testFiguras.java
+++ testFiguras.java
@@ -6,13 +6,17 @@ public class testFiguras {
 		Cuadrado c1 = new  Cuadrado ("Cuadrado", 10);
 		Rectangulo r1 = new Rectangulo ("Rectangulo", 20, 10);
 		Rombo rm1 = new Rombo ("Rombo", 10, 5, 3);
+		Romboide rom1 = new Romboide ("Romboide", 20, 10);
+		Trapecio tra1= new Trapecio ("Trapecio", 14,8,20,8,5,20,14);
+
 		// TODO: Aquí se deben instanciar las otras figuras geométricas
 
 		imprimirCalculo(t1); imprimirCalculo(t2);
 		imprimirCalculo(c1);
 		imprimirCalculo(r1);
 		imprimirCalculo(rm1);
-		
+		imprimirCalculo(rom1);
+		imprimirCalculo(tra1);
 		// TODO: Aquí se debe probar cada figura con el método imprimirCalculo
 		
 	}//main
@@ -30,19 +34,33 @@ public class testFiguras {
 		System.out.println("|El área de ["+c.getName()+"] es: " + c.calcularArea() + 
 				"\n" + "|El perímetro de ["+c.getName()+"] es: " + c.calcularPerimetro());
 		System.out.println("+========================================");
-	}
+	}//imprimirCalculCuadrado
 		public static void imprimirCalculo(Rectangulo r) {
 			System.out.println(r);
 			System.out.println("+========================================");
 			System.out.println("|El área de ["+r.getName()+"] es: " + r.calcularArea() + 
 					"\n" + "|El perímetro de ["+r.getName()+"] es: " + r.calcularPerimetro());
 			System.out.println("+========================================");
-	}
+	}//imprimirCalculoRectangulo
 		public static void imprimirCalculo(Rombo rm1) {
 			System.out.println(rm1);
 			System.out.println("+========================================");
 			System.out.println("|El área de ["+rm1.getName()+"] es: " + rm1.calcularArea() + 
 					"\n" + "|El perímetro de ["+rm1.getName()+"] es: " + rm1.calcularPerimetro());
 			System.out.println("+========================================");
-	}
+	}//imprimirCalculoRombo
+		public static void imprimirCalculo(Romboide rom1) {
+			System.out.println(rom1);
+			System.out.println("+========================================");
+			System.out.println("|El área de ["+rom1.getName()+"] es: " + rom1.calcularArea() + 
+					"\n" + "|El perímetro de ["+rom1.getName()+"] es: " + rom1.calcularPerimetro());
+			System.out.println("+========================================");
+	}//imprimirCalculoRomboide
+		public static void imprimirCalculo(Trapecio tra1) {
+			System.out.println(tra1);
+			System.out.println("+========================================");
+			System.out.println("|El área de ["+tra1.getName()+"] es: " + tra1.calcularArea() + 
+					"\n" + "|El perímetro de ["+tra1.getName()+"] es: " + tra1.calcularPerimetro());
+			System.out.println("+========================================");
+	}//imprimirCalculoTrapecio
 }//class testFiguras
